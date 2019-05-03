49f1648b771fd225a3e36c08190f2e9a403a5294
Jelle Bootsma <j.h.bootsma@student.tue.nl>
2019-05-03 11:02:20 +0200

    Initial Commit

---
+++ sampledata_vis/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\s160262\OneDrive - TU Eindhoven\vakken P&T jaar 3\2IOA0 DBL HTI + webtech\sampledata_vis\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
