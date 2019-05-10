1caafc2637979c73079b0597c699b845ee2c3c49
eviedf <evie.defouw@gmail.com>
2019-05-10 10:55:14 +0200

    links to our different visualizations

--- sampledata_vis/application/urls.py
+++ sampledata_vis/application/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path("", views.homepage, name="homepage")
+ path("", views.homepage, name="homepage"),
+ path('visualization1.html/', views.visualization1),
+ path('visualization2.html/', views.visualization2),
 ]
--- sampledata_vis/application/views.py
+++ sampledata_vis/application/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, HttpResponse
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
 import pandas as pd
@@ -11,10 +11,9 @@ from bokeh.palettes import *
 
 # Create your views here.
 def homepage(request):
-
     G = nx.karate_club_graph()
 
-    plot = Plot(plot_width=400, plot_height=400, x_range=Range1d(-1.1,1.1), y_range= Range1d(-1.1,1.1))
+    plot = Plot(plot_width=400, plot_height=400, x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
     plot.background_fill_color = "#050976"
     plot.background_fill_alpha = 0
     plot.border_fill_color = "#050976"
@@ -22,15 +21,15 @@ def homepage(request):
 
     plot.add_tools(HoverTool(tooltips=None), TapTool(), BoxSelectTool())
 
-    graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0,0))
+    graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
 
     graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="#FCFCFC")
     graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color="#FCFCFC")
     graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color="#22A784")
 
-    graph_renderer.edge_renderer.glyph = MultiLine(line_color= "#FCFCFC", line_alpha=0.8, line_width=5)
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#FCFCFC", line_alpha=0.8, line_width=5)
     graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color="#FCFCFC", line_width=5)
-    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color= "#050976", line_width=5)
+    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color="#050976", line_width=5)
 
     graph_renderer.selection_policy = NodesAndLinkedEdges()
     graph_renderer.inspection_policy = EdgesAndLinkedNodes()
@@ -42,4 +41,64 @@ def homepage(request):
 
     # Return to django homepage with componenents sent as arguments which will then be displayed
     return render_to_response('pages/base.html', dict(script=script, div=div))
+
+def visualization1(request):
+    G = nx.karate_club_graph()
+
+    plot = Plot(plot_width=400, plot_height=400, x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+    plot.background_fill_color = "#050976"
+    plot.background_fill_alpha = 0
+    plot.border_fill_color = "#050976"
+    plot.border_fill_alpha = 0
+
+    plot.add_tools(HoverTool(tooltips=None), TapTool(), BoxSelectTool())
+
+    graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
+
+    graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="#FCFCFC")
+    graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color="#FCFCFC")
+    graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color="#22A784")
+
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#FCFCFC", line_alpha=0.8, line_width=5)
+    graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color="#FCFCFC", line_width=5)
+    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color="#050976", line_width=5)
+
+    graph_renderer.selection_policy = NodesAndLinkedEdges()
+    graph_renderer.inspection_policy = EdgesAndLinkedNodes()
+
+    plot.renderers.append(graph_renderer)
+
+    # store comments
+    script, div = components(plot)
+    return render_to_response('pages/visualization1.html', dict(script=script, div=div))
+
+def visualization2(request):
+    G = nx.karate_club_graph()
+
+    plot = Plot(plot_width=400, plot_height=400, x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+    plot.background_fill_color = "#050976"
+    plot.background_fill_alpha = 0
+    plot.border_fill_color = "#050976"
+    plot.border_fill_alpha = 0
+
+    plot.add_tools(HoverTool(tooltips=None), TapTool(), BoxSelectTool())
+
+    graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
+
+    graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="#FCFCFC")
+    graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color="#FCFCFC")
+    graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color="#22A784")
+
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#FCFCFC", line_alpha=0.8, line_width=5)
+    graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color="#FCFCFC", line_width=5)
+    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color="#050976", line_width=5)
+
+    graph_renderer.selection_policy = NodesAndLinkedEdges()
+    graph_renderer.inspection_policy = EdgesAndLinkedNodes()
+
+    plot.renderers.append(graph_renderer)
+
+    # store comments
+    script, div = components(plot)
+    return render_to_response('pages/visualization2.html', dict(script=script, div=div))
 
