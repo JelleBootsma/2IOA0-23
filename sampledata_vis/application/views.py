036891b013fbd463e5d0164719d416a0337a438e
eviedf <evie.defouw@gmail.com>
2019-05-10 11:06:47 +0200

    adjusts homepage function to not have bokeh code

--- sampledata_vis/application/views.py
+++ sampledata_vis/application/views.py
@@ -11,36 +11,7 @@ from bokeh.palettes import *
 
 # Create your views here.
 def homepage(request):
-    G = nx.karate_club_graph()
-
-    plot = Plot(plot_width=400, plot_height=400, x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
-    plot.background_fill_color = "#050976"
-    plot.background_fill_alpha = 0
-    plot.border_fill_color = "#050976"
-    plot.border_fill_alpha = 0
-
-    plot.add_tools(HoverTool(tooltips=None), TapTool(), BoxSelectTool())
-
-    graph_renderer = from_networkx(G, nx.circular_layout, scale=1, center=(0, 0))
-
-    graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="#FCFCFC")
-    graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color="#FCFCFC")
-    graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color="#22A784")
-
-    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#FCFCFC", line_alpha=0.8, line_width=5)
-    graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color="#FCFCFC", line_width=5)
-    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color="#050976", line_width=5)
-
-    graph_renderer.selection_policy = NodesAndLinkedEdges()
-    graph_renderer.inspection_policy = EdgesAndLinkedNodes()
-
-    plot.renderers.append(graph_renderer)
-
-    # store comments
-    script, div = components(plot)
-
-    # Return to django homepage with componenents sent as arguments which will then be displayed
-    return render_to_response('pages/base.html', dict(script=script, div=div))
+    return render_to_response('pages/base.html')
 
 def coauthorship(request):
     G = nx.karate_club_graph()
