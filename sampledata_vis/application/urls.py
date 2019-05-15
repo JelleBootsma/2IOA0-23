c8a8b80d2259ac3dd9b7e40d22d46e0ae79bfbd9
Eviedf <evie.defouw@gmail.com>
2019-05-15 16:25:47 +0200

    adds html pages

--- sampledata_vis/application/urls.py
+++ sampledata_vis/application/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
- path("", views.homepage, name="homepage"),
- path('visualization1.html/', views.coauthorship),
- path('visualization2.html/', views.weightedgraph),
+ # path("", views.homepage, name="homepage"),
+ url(r'^$', views.homepage, name="home"),
+ url(r'^visualization1/$', views.coauthorship, name="vis1"),
+ url(r'^visualization2/$', views.weightedgraph, name='vis2'),
+ url(r'^data/$', views.data, name="data"),
+ url(r'^FAQ/$', views.faq, name="faq"),
+ # path('visualization1.html/', views.coauthorship),
+ # path('visualization2.html/', views.weightedgraph),
 ]
