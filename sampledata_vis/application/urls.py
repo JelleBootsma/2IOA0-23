0f7639930dc2f98145bd71407a41ba46afbb3b68
yana <YanaOnushkina>
2019-05-18 14:06:19 +0200

    Adding non working visualization3 and changing paths to .html

--- sampledata_vis/application/urls.py
+++ sampledata_vis/application/urls.py
@@ -5,10 +5,12 @@ from . import views
 urlpatterns = [
  # path("", views.homepage, name="homepage"),
  url(r'^$', views.homepage, name="home"),
- url(r'^visualization1/$', views.coauthorship, name="vis1"),
- url(r'^visualization2/$', views.weightedgraph, name='vis2'),
+ url(r'^visualization1.html/$', views.coauthorship, name="vis1"),
+ url(r'^visualization2.html/$', views.weightedgraph, name='vis2'),
+url(r'^visualization3.html/$', views.adjacencymatrix, name='vis3'),
  url(r'^data/$', views.data, name="data"),
  url(r'^FAQ/$', views.faq, name="faq"),
  # path('visualization1.html/', views.coauthorship),
  # path('visualization2.html/', views.weightedgraph),
+ # path('visualization3.html/', views.adjacencymatrix),
 ]
